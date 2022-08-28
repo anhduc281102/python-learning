a80868b2991d48ecaaf0ddc42c59c6fbef234a13
anhduc281102 <jamieanhduc281102@gmail.com>
2022-08-28 17:14:06 +0700

    themfilebaitap

--- class_learn.py
+++ class_learn.py
@@ -1,14 +1,45 @@
-from socket import CAN_BCM_RX_READ
+        
+from copy import copy
 
 
-class baido:
-	def __init__(self, phuongtien, sove, camera ):
-		self.phuongtien = phuongtien
-        self.sove = sove
-        self.camera = camera
-		 	
+class Mess:
+    def __init__(self, noidung, from_, to):
+        self.from_ = from_
+        self.to = to 
+        self.noidung = noidung  
 
-class xe:
-	def __init__(self, loaixe, bienso ):
-		self.loaixe = loaixe
-        self.bienso = bienso
+# tmp = {
+#     "from":None,
+#     "to":None,
+#     'noiding':''
+# } 
+
+class Phone:
+    def __init__(self, ten , belong, danhsach = []):
+        self.ten = ten
+        self.belongto = belong
+        self.danhsach = danhsach
+    
+    def send_mess(self, noidung, to ):
+        mess_sender = Mess(noidung, self.belongto, to)
+        self.danhsach.append(mess_sender)
+        receiver_phone = to.phone
+        mess_receiver = copy(mess_sender)
+        receiver_phone.append(mess_receiver)
+        print("done!!")
+
+class Person:
+    def __init__(self, name, phone  ):
+        self.name = name 
+        self.phone = phone 
+    
+    def send(self, noidung, to):
+        self.phone.send_mess(noidung, to)
+    # def bam_dien_thoai(self, noidung,to): 
+        #self.phone.sendmess(noidung,to )
+    #def readmess(self, bywhom):
+        # self.phone.sendmess(noidung,to )
+
+
+
+        
