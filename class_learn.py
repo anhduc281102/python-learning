105b81f2ed79ddd7ff88004e9b02d542f2fc8583
anhduc281102 <jamieanhduc281102@gmail.com>
2022-08-28 17:13:16 +0700

    them file bai tap

---
+++ class_learn.py
@@ -0,0 +1,14 @@
+from socket import CAN_BCM_RX_READ
+
+
+class baido:
+	def __init__(self, phuongtien, sove, camera ):
+		self.phuongtien = phuongtien
+        self.sove = sove
+        self.camera = camera
+		 	
+
+class xe:
+	def __init__(self, loaixe, bienso ):
+		self.loaixe = loaixe
+        self.bienso = bienso
